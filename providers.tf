328948f009f5fa0d66a6f7e4975d869f16aeddf7
Rohit-singh-gusain <rohitgusain9930@gmail.com>
2026-06-14 22:42:32 +0530

    updated .gitignore

---
+++ providers.tf
@@ -0,0 +1,17 @@
+
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      
+      ManagedBy   = "terraform"
+    }
+  }
+}
+
+
+
+
+
+
---
+++ terraform.tfvars
@@ -0,0 +1,4 @@
+aws_region = "us-east-1"
+
+bucket_name = "rohit-gusain-portfolio6789"
+
